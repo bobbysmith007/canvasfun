099344d328895e4487f08d274b1f9de10a830cb9
Russ Tyndall <russ@acceleration.net>
2010-09-10 11:34:16 -0400

    trying to get gears to generate new meshed gears

--- gears.js
+++ gears.js
@@ -8,26 +8,35 @@ context.clear = function(){
   context.save();
 
 };
+var mergeHash = function(h1, h2){
+  h1 = h1||{};
+  h2 = h2||{};
+  for(key in h2)if (!h1[key]) h1[key] = h2[key];
+  return h1;
+};
 
 var degToRad = 0.01745329525;
 var Gear = function(opts){
   opts = opts||{};
-  this.x = opts.x || width/2;
-  this.y = opts.y || height/2;
-  this.diameter = opts.diameter || 100;
-  this.teethAngle = opts.teethAngle || 5*degToRad;
+  // Set defaults and opts
+  mergeHash(this, mergeHash(opts, {
+    x: width/2,
+    y: height/2,
+    diameter: 100,
+    teethAngle: 10*degToRad,
+    teethCount: 12,
+    strokeStyle: 'rgba(0,192,0,255)',
+    rotation: 0,
+    rotationSpeed: 1
+  }));
+  this.opts = opts;
   this.teethLength = opts.teethLength || this.diameter/3;
-  this.teethCount = opts.teethCount || 12;
-  this.teethAngle = (opts.teethAngle || 5) * degToRad;
   this.teethDiameter = this.diameter+this.teethLength;
   this.teethTheta = (180 / this.teethCount);
   this.teethRatio = this.teethDiameter/this.diameter;
   this.teethSlip = Math.sin(this.teethAngle) * (this.teethDiameter-this.diameter);
-  this.strokeStyle = opts.strokeStyle || 'rgba(0,192,0,255)';
-  this.fillStyle = opts.fillStyle || null;
   this.hubDiameter = opts.hubDiameter || this.diameter / 2;
-  this.rotation = opts.rotation||0;
-  this.rotationSpeed = opts.rotationSpeed||1;
+
   // slip without angle
   // teethSlip = (teethTheta - (teethTheta / teethRatio ));
   var g = this;
@@ -35,7 +44,7 @@ var Gear = function(opts){
     context.beginPath();
     if(this.strokeStyle) context.strokeStyle = this.strokeStyle;
     if(this.fillStyle) context.fillStyle = this.fillStyle;
-    context.arc(0,0,this.hubDiameter,0,360*degToRad);
+    context.arc(0,0,this.hubDiameter,0,360*degToRad, false);
     context.stroke();
     context.closePath();
   };
@@ -58,7 +67,7 @@ var Gear = function(opts){
       startAngle = (i-1)*degToRad;
       endAngle = i*degToRad;
       var d = teething ? this.teethDiameter : this.diameter;
-      context.arc(0,0,d,startAngle,endAngle);
+      context.arc(0,0,d,startAngle,endAngle, false);
       if(teethAngleCnt > this.teethTheta){
 	teething = !teething;
 	//handles distributing fractional degrees among all the teeth
@@ -73,18 +82,39 @@ var Gear = function(opts){
     context.closePath();
     context.restore();
   };
+  this.makeNewAttachedGear = function(opts){
+    opts = mergeHash(opts, g.opts );
+    opts = mergeHash(opts, {relativeAngle:0, gearPadding:6, relativeSize:1});
+    if(opts.diameter == g.diameter && opts.relativeSize!=1){
+      opts.diameter = opts.relativeSize*g.diameter;
+      opts.teethCount = Math.floor(g.teethCount*opts.relativeSize);
+    }
+    if(opts.rotationSpeed == g.rotationSpeed)
+      opts.rotationSpeed = -(g.diameter / (opts.diameter||g.diameter)) * g.rotationSpeed;
+    if(opts.x == g.x && opts.y == g.y){
+      var dist = (g.diameter + (opts.teethDiameter || g.teethDiameter))+opts.gearPadding;
+      opts.x = g.x+(dist * Math.cos(opts.relativeAngle * degToRad));
+      opts.y = g.y+(dist * Math.sin(opts.relativeAngle * degToRad));
+    }
+    if(opts.rotation == g.rotation)
+      opts.rotation = g.rotation + (g.teethTheta - g.teethSlip/2)/2;
+    var g2 = new Gear(opts);
+    return g2;
+  };
 };
 
 context.clear();
 var g = new Gear({x: 150, y:150});
-var g2 = new Gear({x: 325, y:325,
-                   rotation:g.teethTheta/2,
-                   rotationSpeed : -1 * g.rotationSpeed});
+var g2 = g.makeNewAttachedGear();
+var g3 = g.makeNewAttachedGear({relativeAngle: 90});
+//var g4 = g2.makeNewAttachedGear({relativeAngle: 90});
 var spin = function(){
   context.clear();
   g.draw();
   g2.draw();
-  window.setTimeout(spin, 25);
+  g3.draw();
+//  g4.draw();
+//  window.setTimeout(spin, 25);
 };
 window.setTimeout(spin, 25);
 
