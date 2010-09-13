7c61493527b2ee4dd15bf79283074a5a322b6fb5
Russ Tyndall <russ@acceleration.net>
2010-09-13 10:05:17 -0400

    added animation micro framework and fill gears / crop out hub

--- gears.js
+++ gears.js
@@ -11,19 +11,64 @@ context.clear = function(){
 var mergeHash = function(h1, h2){
   h1 = h1||{};
   h2 = h2||{};
-  for(key in h2)if (!h1[key]) h1[key] = h2[key];
+  for(key in h2)if (h1[key]==null) h1[key] = h2[key];
   return h1;
 };
 
+var Frame = function(opts){
+  opts = opts||{};
+  if(opts[0]) this.drawables = opts;
+  else this.drawables = opts.drawables || [];
+  this.draw = function(){
+    context.clear();
+    for(var j=0, drawable ; drawable=this.drawables[j] ; j++)
+      drawable.draw();
+  };
+};
+
+var Animation = function(opts) {
+  opts = opts||{};
+  if(opts && opts[0]) this.frames = opts;
+  else this.frames = opts.frames || [];
+  this.iterations = null;
+  this.timing = opts.timing || 25;
+  this.framePointer = 0;
+  var animation = this;
+  animation.spin = function(){
+    if(!animation.iterations || animation.iterations > 0){
+      if(animation.frames[animation.framePointer]){
+	animation.frames[animation.framePointer].draw();
+	animation.framePointer++;
+      }else{
+	animation.framePointer = 0;
+	var frame = animation.frames[animation.framePointer];
+	frame && frame.draw();
+      }
+      animation.iterations--;
+      window.setTimeout(animation.spin, animation.timing);
+    }
+  };
+  this.go = function (iterations){
+    this.iterations = iterations || null;
+    window.setTimeout(animation.spin, animation.timing);
+  };
+  this.stop = function(){this.iterations=-1;};
+};
+
+/////////////////////////////////////////////////
+
+
 var degToRad = 0.01745329525;
+var Drawable = { draw:function(){} };
 var Gear = function(opts){
+  this.prototype = Drawable;
   opts = opts||{};
   // Set defaults and opts
   mergeHash(this, mergeHash(opts, {
     x: width/2,
     y: height/2,
-    diameter: 100,
-    teethAngle: 10*degToRad,
+    diameter: 30,
+    teethAngle: 30*degToRad,
     teethCount: 12,
     strokeStyle: 'rgba(0,192,0,255)',
     rotation: 0,
@@ -41,19 +86,22 @@ var Gear = function(opts){
   // teethSlip = (teethTheta - (teethTheta / teethRatio ));
   var g = this;
   this._drawHub = function(){
+    var tmp = context.globalCompositeOperation;
+    context.globalCompositeOperation = "destination-out";
     context.beginPath();
     if(this.strokeStyle) context.strokeStyle = this.strokeStyle;
     if(this.fillStyle) context.fillStyle = this.fillStyle;
     context.arc(0,0,this.hubDiameter,0,360*degToRad, false);
     context.stroke();
+    context.fill();
     context.closePath();
+    context.globalCompositeOperation = tmp;
   };
   this.draw = function(){
     context.save();
     this.rotation+=this.rotationSpeed;
     context.translate (this.x, this.y);
     context.rotate(this.rotation*degToRad);
-    this._drawHub();
     context.beginPath();
     if(this.strokeStyle) context.strokeStyle = this.strokeStyle;
     if(this.fillStyle) context.fillStyle = this.fillStyle;
@@ -79,12 +127,15 @@ var Gear = function(opts){
     }
     context.lineTo(this.diameter, 0);
     context.stroke();
+    if(this.fillStyle) context.fill();
     context.closePath();
+    this._drawHub();
     context.restore();
+    return true;
   };
   this.makeNewAttachedGear = function(opts){
     opts = mergeHash(opts, g.opts );
-    opts = mergeHash(opts, {relativeAngle:0, gearPadding:6, relativeSize:1});
+    opts = mergeHash(opts, {relativeAngle:0, gearPadding: (.1 * g.diameter), relativeSize:1});
     if(opts.diameter == g.diameter && opts.relativeSize!=1){
       opts.diameter = opts.relativeSize*g.diameter;
       opts.teethCount = Math.floor(g.teethCount*opts.relativeSize);
@@ -97,24 +148,40 @@ var Gear = function(opts){
       opts.y = g.y+(dist * Math.sin(opts.relativeAngle * degToRad));
     }
     if(opts.rotation == g.rotation)
-      opts.rotation = g.rotation + (g.teethTheta - g.teethSlip/2)/2;
+      if(g.parent) opts.rotation = g.parent.rotation;
+      else opts.rotation = g.rotation + g.teethTheta/2;
+    opts.parent = g;
     var g2 = new Gear(opts);
     return g2;
   };
 };
 
 context.clear();
-var g = new Gear({x: 150, y:150});
+
+/*
+var g = new Gear({x: 100, y:100, fillStyle:'rgb(0,128,0)'});
 var g2 = g.makeNewAttachedGear();
 var g3 = g.makeNewAttachedGear({relativeAngle: 90});
-//var g4 = g2.makeNewAttachedGear({relativeAngle: 90});
-var spin = function(){
-  context.clear();
-  g.draw();
-  g2.draw();
-  g3.draw();
-//  g4.draw();
-//  window.setTimeout(spin, 25);
-};
-window.setTimeout(spin, 25);
+var g4 = g2.makeNewAttachedGear({relativeAngle: 45});
+var g5 = g2.makeNewAttachedGear({relativeAngle: 360-45});
+var g6 = g5.makeNewAttachedGear({relativeAngle: 45});
+var frame = new Frame([g, g2, g3, g4, g5, g6]);
+*/
+/*   Tile canvas */
+var frame = new Frame();
+var g = new Gear({x: 0, y:0, fillStyle:'rgb(0,128,0)', rotationSpeed:2});
+frame.drawables.push(g);
+for(var i=0 ; i <= 12 ; i++)
+  for(var j=0 ; j <= 12 ; j++){
+    angle = i%2==0?(j%2==0?45:(360-45)):(j%2==1?(180+45):(180-45));
+    frame.drawables.push(g = g.makeNewAttachedGear({relativeAngle:angle}));
+  }
+
+
+
+
+
+var animation = new Animation({frames:[frame], timing:5});
+animation.go(10000);
+
 
