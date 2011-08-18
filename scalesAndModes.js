bc9cf7258488840d4b7c7b7462683c88e37ce152
Russ Tyndall <russ@acceleration.net>
2011-08-18 16:28:10 -0400

    added standard notation

--- scalesAndModes.js
+++ scalesAndModes.js
@@ -1,5 +1,9 @@
 var canvas = document.getElementById('canvas');
+var trebleStaff = document.getElementById('treble');
+var bassStaff = document.getElementById('bass');
 var context = canvas.getContext('2d');
+var treblecontext = trebleStaff.getContext('2d');
+var basscontext = bassStaff.getContext('2d');
 var Ab='Ab',A='A',Bb='Bb', B='B',C='C',Db='Db',D='D',Eb='Eb',
     E='E',Fb='Fb',F='F',Gb='Gb',G='G', T=true, X=false,
     Ionian = 'Ionian (I) - Major', Dorian = 'Dorian (II)',
@@ -33,6 +37,14 @@ context.clear = function(){
   context.restore();
   context.clearRect(0,0,width,height);
   context.save();
+};
+var clearStaves = function(){
+  treblecontext.restore();
+  treblecontext.clearRect(0,0,width,100);
+  treblecontext.save();
+  basscontext.restore();
+  basscontext.clearRect(0,0,width,100);
+  basscontext.save();
 };
 var degToRad = 0.01745329525;
 var dodecaDeg = (360/12);
@@ -118,17 +130,78 @@ function noteSummary(scale){
   for(i=0; i<12 ;i++)
     if(ringref(scale, i, MODE))
       out.push(ringref(chromaticScale, i, KEY));
-  return out.join(' - ');
+  return out;
+}
+
+var staffs={};
+function drawStaff(scale){
+  staffs.treblerenderer = new Vex.Flow.Renderer(trebleStaff, Vex.Flow.Renderer.Backends.CANVAS);
+  staffs.bassrenderer = new Vex.Flow.Renderer(bassStaff, Vex.Flow.Renderer.Backends.CANVAS);
+  staffs.treblestaff = new Vex.Flow.Stave(10, 0, 550);
+  staffs.bassstaff = new Vex.Flow.Stave(10, 0, 550);
+  // Add a treble clef
+
+  function create_4_4_voice(len) {
+    return new Vex.Flow.Voice({
+      num_beats: len,
+      beat_value: 4,
+      resolution: Vex.Flow.RESOLUTION
+    });
+  }
+  var underStart=3;
+  function note(letter, clef){
+    var under=underStart;
+    if(clef == 'bass')under--;
+    var note = new Vex.Flow.StaveNote({
+      clef:clef,
+      keys: [letter.toUpperCase()+"/"+under],
+      duration: "q"
+    });
+    if ( letter[1] && letter[1]=='b' ){
+      note.addAccidental(0, new Vex.Flow.Accidental("b"));
+    }
+    return note;
+  }
+
+  var inNotes=noteSummary(scale),trebleNotes=[],bassNotes=[], i, letter, lastLetter;
+  for(i=0; letter=inNotes[i] ;i++, lastLetter=letter){
+    // keep the scale ascending
+    if((lastLetter == null || lastLetter < C)
+       && letter >= C) underStart++;
+    trebleNotes.push(note(letter, "treble",i ));
+    bassNotes.push(note(letter, "bass", i ));
+  }
+  staffs.tvoice = create_4_4_voice(trebleNotes.length).addTickables(trebleNotes);
+  staffs.bvoice = create_4_4_voice(bassNotes.length).addTickables(bassNotes);
+
+  staffs.treblestaff.addClef("treble");
+  // staffs.treblestaff.addKeySignature(ringref(chromaticScale, KEY));
+  staffs.treblestaff.setContext(treblecontext).draw();
+  staffs.tformatter = new Vex.Flow.Formatter().joinVoices([staffs.tvoice])
+    .format([staffs.tvoice], 500);
+  staffs.bformatter = new Vex.Flow.Formatter().joinVoices([staffs.bvoice])
+    .format([staffs.bvoice], 500);
+  staffs.tvoice.draw(treblecontext, staffs.treblestaff);
+
+  staffs.bassstaff.addClef("bass");
+  console.log( ringref(chromaticScale, KEY) );
+  // staffs.bassstaff.addKeySignature(ringref(chromaticScale, KEY));
+  staffs.bassstaff.setContext(basscontext).draw();
+  staffs.bvoice.draw(basscontext, staffs.bassstaff);
+
+
 }
 
 function drawRings(){
   context.clear();
+  clearStaves();
   drawRingText(180, modeNames, MODE);
   drawRing(220, majorScale, MODE);
   drawRingText(240, chromaticScale, KEY);
   $('.mode .current').text( ringref(modeNames, MODE) );
   $('.key .current').text( ringref(chromaticScale, KEY) );
-  $('.notes .current').text(noteSummary(majorScale, MODE+KEY));
+  $('.notes .current').text(noteSummary(majorScale, MODE+KEY).join(' - '));
+  drawStaff(majorScale);
 }
 
 drawRings();
